297b101c96eef61d634ef39082c58b27d47a8d05
shashi <shashi2017razz@gmail.com>
2026-05-19 10:32:38 +0530

    added .sequelizerc

---
+++ src/config/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  development: {
+    username: "root",
+    password: "@Xprofess0r123",
+    database: "flight_search_db",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  test: {
+    username: "root",
+    password: "@Xprofess0r123",
+    database: "flight_search_db_test",
+    host: "127.0.0.1",
+    dialect: "mysql"
+  },
+  production: {
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    dialect: "mysql"
+  }
+};
